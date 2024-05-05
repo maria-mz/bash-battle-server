3f789fd387c82860f2f7f32f3ba83075d4308896
Maria Zaman <mariazaman2@yahoo.com>
2024-05-05 14:41:03 -0700

    add server config

---
+++ config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type ServerConfig struct {
+	Host string `json:"host"`
+	Port uint16 `json:"port"`
+}
+
+func LoadConfig() (ServerConfig, error) {
+	var config ServerConfig
+
+	file, err := os.Open("config.json")
+
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maria-mz/bash-battle
+
+go 1.22.2
